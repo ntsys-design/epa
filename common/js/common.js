e0092d076462f89bd29d5a7966b60d00ad573eea
ntsys-design <stary92@daum.net>
2023-05-19 13:37:34 +0900

    #230519 1

--- common/js/common.js
+++ common/js/common.js
@@ -1,35 +1,34 @@
 $(document).ready(function () {
-    $(".tab_con_list > div").first().addClass('active')
-    $(".tab_list > li").on('click' ,function (e) {
-        e.preventDefault();
-        $(".tab_list > li").removeClass("active");
-        $(this).addClass("active");
-        $(".tab_con_list > div").removeClass("active");
-        $(".tab_con_list > div").eq($(this).index()).addClass("active");
-    });
-
-    $(".qline").click(function () {
-        if ($(this).hasClass("opened")) {
-            $(this).removeClass("opened");
-            $(this).siblings().slideUp();
-        } else {
-            $(".aline").slideUp();
-            $(".qline").removeClass("opened");
-            $(this).addClass("opened");
-            $(this).siblings().slideDown();
-        }
-    });
+  $(".tab_con_list > div").first().addClass("active");
+  $(".tab_list > li").on("click", function (e) {
+    e.preventDefault();
+    $(".tab_list > li").removeClass("active");
+    $(this).addClass("active");
+    $(".tab_con_list > div").removeClass("active");
+    $(".tab_con_list > div").eq($(this).index()).addClass("active");
+  });
+  
+  //공용 셀렉트
+  $(".ulsel_btn").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("open")) {
+      $(this).removeClass("open");
+      $(this).siblings().fadeOut(100);
+    } else {
+      $(this).addClass("open");
+      $(this).siblings().fadeIn(100);
+    }
+  });
 });
 
-    
-    // 회원탈퇴 팝업열기
-    $(document).on("click", ".exp_btn", function (e) {
-        var target = $(".popup_wrap");
-        $(target).parent().addClass("poped");
-        $(".wrapper").addClass("modaled");
-    });
+// 회원탈퇴 팝업열기
+$(document).on("click", ".exp_btn", function (e) {
+  var target = $(".popup_wrap");
+  $(target).parent().addClass("poped");
+  $(".wrapper").addClass("modaled");
+});
 
-    $(".btn_close").on("click", function () {
-        $(".popup_modal").removeClass("poped");
-        $(".wrapper").removeClass("modaled");
-    });
+$(".btn_close").on("click", function () {
+  $(".popup_modal").removeClass("poped");
+  $(".wrapper").removeClass("modaled");
+});
