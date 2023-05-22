0b4a5e4ad8a512cce3969b3837b9b2c1cadd0f61
ntsys-design <stary92@daum.net>
2023-05-22 13:54:48 +0900

    123

--- common/js/common.js
+++ common/js/common.js
@@ -1,3 +1,15 @@
+
+$(window).scroll(function () {
+  let scrTop = $(this).scrollTop()
+  if (scrTop > 80) {
+    $('header').addClass("scrolled");
+    // $('header .logowrap h1 img').attr('src','../common/images/header_logo_c_c.png');
+  } else {
+    $('header').removeClass("scrolled");
+    // $('header .logowrap h1 img').attr('src','../common/images/header_logo_c.png');
+  }
+})
+
 $(document).ready(function () {
   // 헤더 scrolled
   // var headerTop = $('header').scrollTop();
@@ -12,16 +24,6 @@ $(document).ready(function () {
   //   }
   // })
 
-  $(window).scroll(function () {
-    let scrTop = $(this).scrollTop()
-    if (scrTop > 80) {
-      $('header').addClass("scrolled");
-      $('header .logowrap h1 img').attr('src','../common/images/header_logo_c.png');
-    } else {
-      $('header').removeClass("scrolled");
-      $('header .logowrap h1 img').attr('src','../common/images/header_logo.png');
-    }
-  })
 
     $(".tab_con_list > div").first().addClass('active')
     $(".tab_list > li").on('click' ,function (e) {
