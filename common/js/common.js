f6b1a9edeb0ce967c7c189064f2d0ad50087f9a1
ntsys-design <stary92@daum.net>
2023-05-23 16:23:58 +0900

    font changed

--- common/js/common.js
+++ common/js/common.js
@@ -1,4 +1,5 @@
 
+
 $(window).scroll(function () {
   let scrTop = $(this).scrollTop()
   if (scrTop > 80) {
@@ -26,13 +27,14 @@ $(document).ready(function () {
 
 //메인 News탭
   $(".newscon_list > li").first().addClass('active')
-  $(".swiperNst ul li").on('click' ,function (e) {
+
+ /*  $(".newstab_list > li").on('click' ,function (e) {
       e.preventDefault();
-      $(".swiperNst ul li").removeClass("active");
-      $(this).addClass("active");
       $(".newscon_list > li").removeClass("active");
+      $(".newstab_list > li").removeClass("active");
+      $(this).addClass("active");
       $(".newscon_list > li").eq($(this).index()).addClass("active");
-  });
+  }); */
 
 //공용 탭
     $(".tab_con_list > div").first().addClass('active')
